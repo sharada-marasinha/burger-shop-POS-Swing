f7399e12cd8b147e1a9b7ffbec505cd5ec5acfaa
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2024-02-13 21:45:35 +0530

    adding button

--- src/View/HomePage.java
+++ src/View/HomePage.java
@@ -5,6 +5,8 @@
  */
 package View;
 
+import java.awt.Font;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 /**
@@ -13,11 +15,26 @@ import javax.swing.JFrame;
  */
 public class HomePage extends JFrame{
     
+    private JButton btnAddCustomer;
+    private JButton btnPlaceOrder;
+    private JButton btnSearch;
+    private JButton btnViewOrder;
+    private JButton btnUpdateOrder;
+    
     public HomePage(){
         setTitle("Home Page");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(500,500);
         setLocationRelativeTo(null);
         setVisible(true);
+        setLayout(null);
+        
+        btnAddCustomer=new JButton("Add Customer");
+        btnAddCustomer.setFont(new Font("",Font.PLAIN,15));
+        btnAddCustomer.setBounds(100, 100, 300, 40);
+        
+        
+        add(btnAddCustomer);
+        
     }
 }
