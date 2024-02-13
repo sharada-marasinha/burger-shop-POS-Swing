2e4c690fe946fde29bdd51f83f8c810fa0175a77
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2024-02-13 21:12:14 +0530

    adding DB

--- src/DB/OrderList.java
+++ src/DB/OrderList.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package DB;
 
 /**
  *
  * @author shara
  */
-public class Main {
+public class OrderList {
     
 }
